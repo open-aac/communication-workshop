ebee9c17d67dd401b2bc88c85fc0893bc4395445
Brian Whitmer <brian.whitmer@gmail.com>
2017-08-11 12:25:11 -0600

    allow unauthorized viewing of word data

--- lib/json_api/word.rb
+++ lib/json_api/word.rb
@@ -14,19 +14,20 @@ module JsonApi::Word
     
     if args[:permissions]
       json['permissions'] = word.permissions_for(args[:permissions])
-      if word.data
-        # other data to include
-        WordData::STRING_PARAMS.each do |param|
-          json[param] = word.data[param].to_s if word.data[param]
-        end
-        WordData::OBJ_PARAMS.each do |param|
-          json[param] = word.data[param] if word.data[param]
-        end
-        json['related_identifiers'] = word.data['all_user_identifiers']
-        json['approved_user_identifiers'] = word.data['approved_user_identifiers']
-        json['revisions'] = word.data['revisions'] || []
-      end
     end
+    if word.data
+      # other data to include
+      WordData::STRING_PARAMS.each do |param|
+        json[param] = word.data[param].to_s if word.data[param]
+      end
+      WordData::OBJ_PARAMS.each do |param|
+        json[param] = word.data[param] if word.data[param]
+      end
+      json['related_identifiers'] = word.data['all_user_identifiers']
+      json['approved_user_identifiers'] = word.data['approved_user_identifiers']
+      json['revisions'] = word.data['revisions'] || []
+    end
+    
     json
   end
 end
