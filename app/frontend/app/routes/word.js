ac452eeb5c06d62a597802e073519a2f90a392bd
Brian Whitmer <brian.whitmer@gmail.com>
2017-08-11 12:34:33 -0600

    more consistent record loading

--- app/frontend/app/routes/word.js
+++ app/frontend/app/routes/word.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.findRecord('word', params.word + ":" + params.locale);
+    return this.store.findRecord('word', params.word + ":" + params.locale).then(function(res) {
+      if(!res.get('permissions')) {
+        return res.reload();
+      } else {
+        return res;
+      }
+    });
   }
 });
