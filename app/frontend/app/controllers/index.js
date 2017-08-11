216aa0c90e87889047355a87c873dd85b7b83478
Brian Whitmer <brian.whitmer@gmail.com>
2017-08-11 12:11:15 -0600

    update index

--- app/frontend/app/controllers/index.js
+++ app/frontend/app/controllers/index.js
@@ -9,6 +9,15 @@ export default Ember.Controller.extend({
     }, function() {
       _this.set('words', {error: true});
     });
+  },
+  load_categories: function() {
+    var _this = this;
+    _this.set('categories', {loading: true});
+    this.store.query('category', {}).then(function(res) {
+      _this.set('categories', res);
+    }, function() {
+      _this.set('categories', {error: true});
+    });
   }
 });
 
--- app/frontend/app/routes/index.js
+++ app/frontend/app/routes/index.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController: function(controller, model) {
     controller.load_words();
+    controller.load_categories();
   }
 });
