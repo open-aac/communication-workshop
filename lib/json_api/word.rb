914eac25d34a2cf6f10287800a7d4d3a1f052b13
Brian Whitmer <brian.whitmer@gmail.com>
2018-01-26 14:41:56 -0700

    faster rendering of paginated words

--- lib/json_api/word.rb
+++ lib/json_api/word.rb
@@ -2,8 +2,8 @@ module JsonApi::Word
   extend JsonApi::Json
   
   TYPE_KEY = 'word'
-  DEFAULT_PAGE = 25
-  MAX_PAGE =50
+  DEFAULT_PAGE = 50
+  MAX_PAGE =100
     
   def self.build_json(word, args={})
     json = {}
@@ -14,29 +14,33 @@ module JsonApi::Word
     word.data ||= {}
     json['pending_revisions'] = (word.data['revisions'] || []).length > 0
     
+    strings = ['border_color', 'background_color', 'description']
+    objs = ['image']
     if args[:permissions]
       json['permissions'] = word.permissions_for(args[:permissions])
-    end
-    if word.data
-      # other data to include
-      WordData::STRING_PARAMS.each do |param|
-        json[param] = word.data[param].to_s if word.data[param]
+      if word.data
+        # other data to include
+        strings = WordData::STRING_PARAMS
+        objs = WordData::OBJ_PARAMS
+        json['related_identifiers'] = word.data['all_user_identifiers']
+        json['approved_user_identifiers'] = word.data['approved_user_identifiers']
+        json['revisions'] = word.data['revisions'] || []
       end
-      WordData::OBJ_PARAMS.each do |param|
-        if word.data[param]
-          if word.data[param].is_a?(Array)
-            word.data[param].each do |obj|
-              if obj['id'] && !obj['id'].match(/:/)
-                obj['id'] = "#{word.global_id}:#{obj['id']}"
-              end
+    end
+    strings.each do |param|
+      json[param] = word.data[param].to_s if word.data[param]
+    end
+    objs.each do |param|
+      if word.data[param]
+        if word.data[param].is_a?(Array)
+          word.data[param].each do |obj|
+            if obj['id'] && !obj['id'].match(/:/)
+              obj['id'] = "#{word.global_id}:#{obj['id']}"
             end
           end
-          json[param] = word.data[param]
         end
+        json[param] = word.data[param]
       end
-      json['related_identifiers'] = word.data['all_user_identifiers']
-      json['approved_user_identifiers'] = word.data['approved_user_identifiers']
-      json['revisions'] = word.data['revisions'] || []
     end
     
     json
