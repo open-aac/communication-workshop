1d87647750f28c886393cf894d3933c4899946ad
Brian Whitmer <brian.whitmer@gmail.com>
2018-01-26 15:30:59 -0700

    show partial results on words list

--- app/frontend/app/controllers/words.js
+++ app/frontend/app/controllers/words.js
@@ -5,7 +5,9 @@ export default Ember.Controller.extend({
   load_words: function() {
     var _this = this;
     _this.set('words', {loading: true});
-    pages.all('word', {sort: 'alpha'}).then(function(res) {
+    pages.all('word', {sort: 'alpha'}, function(res) {
+      _this.set('words', res);
+    }).then(function(res) {
       _this.set('words', res);
     }, function() {
       _this.set('words', {error: true});
