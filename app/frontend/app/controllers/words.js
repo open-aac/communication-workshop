87d1b69ce7f279e5c5eeab30b592d54be2adc304
Brian Whitmer <brian.whitmer@gmail.com>
2018-01-30 12:40:56 -0700

    append instead of rewriting the list

--- app/frontend/app/controllers/words.js
+++ app/frontend/app/controllers/words.js
@@ -5,10 +5,22 @@ export default Ember.Controller.extend({
   load_words: function() {
     var _this = this;
     _this.set('words', {loading: true});
+    var append = function(results) {
+      var list = _this.get('words');
+      if(!_this.get('words.length')) {
+        list = [];
+        _this.set('words', list);
+      }
+      results.forEach(function(word) {
+        if(!list.find(function(w) { return w.get('id') == word.get('id'); })) {
+          list.pushObject(word);
+        }
+      });
+    };
     pages.all('word', {sort: 'alpha'}, function(res) {
-      _this.set('words', res);
+      append(res);
     }).then(function(res) {
-      _this.set('words', res);
+      append(res);
     }, function() {
       _this.set('words', {error: true});
     });
