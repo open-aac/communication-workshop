65bff3a11156771b4d91bea703875c2aa5c4c99a
Brian Whitmer <brian.whitmer@gmail.com>
2018-01-30 12:00:23 -0700

    close modals on redirect

--- app/frontend/app/routes/application.js
+++ app/frontend/app/routes/application.js
@@ -6,5 +6,10 @@ export default Ember.Route.extend({
   setupController: function(controller, model) {
     controller.load_user();
     modal.setup(this);
+  },
+  actions: {
+    willTransition: function() {
+      modal.close();
+    }
   }
 });
